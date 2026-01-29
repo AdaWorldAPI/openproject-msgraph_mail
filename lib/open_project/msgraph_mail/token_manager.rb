d57663fabbfdf7ade900dac103922acb4d5e39ca
AdaWorldAPI <jan@exo.red>
2026-01-29 12:17:23 +0100

    fix: Use ::Mutex to avoid OpenProject::Mutex shadow

--- lib/open_project/msgraph_mail/token_manager.rb
+++ lib/open_project/msgraph_mail/token_manager.rb
@@ -8,7 +8,6 @@
 # modify it under the terms of the GNU General Public License version 3.
 #++
 
-require "oauth2"
 require "json"
 require "net/http"
 
@@ -25,73 +24,57 @@ module OpenProject
 
       class TokenError < StandardError; end
 
-      class << self
-        def instance
-          @instance ||= new
-        end
-
-        # Delegate methods to instance
-        def access_token(config)
-          instance.access_token(config)
-        end
-
-        def clear_cache!
-          instance.clear_cache!
-        end
-      end
-
-      def initialize
-        @mutex = Mutex.new
-        @tokens = {}
+      def initialize(tenant_id:, client_id:, client_secret:)
+        @tenant_id = tenant_id
+        @client_id = client_id
+        @client_secret = client_secret
+        # Use ::Mutex to explicitly reference Ruby's built-in Mutex class
+        # (OpenProject has a module called OpenProject::Mutex which shadows it)
+        @mutex = ::Mutex.new
+        @cached_token = nil
+        @token_expires_at = nil
       end
 
       # Get a valid access token, refreshing if necessary
-      # @param config [Configuration] MS Graph configuration
       # @return [String] Access token
-      def access_token(config)
-        cache_key = cache_key_for(config)
-
+      def access_token
         @mutex.synchronize do
-          cached = @tokens[cache_key]
-
-          if cached && !token_expired?(cached)
+          if @cached_token && !token_expired?
             Rails.logger.debug { "MS Graph Mail: Using cached token" }
-            return cached[:access_token]
+            return @cached_token
           end
 
           Rails.logger.info "MS Graph Mail: Fetching new access token"
-          token_data = fetch_token(config)
-          @tokens[cache_key] = token_data
-          token_data[:access_token]
+          fetch_token
+          @cached_token
         end
       end
 
       def clear_cache!
-        @mutex.synchronize { @tokens.clear }
+        @mutex.synchronize do
+          @cached_token = nil
+          @token_expires_at = nil
+        end
       end
 
       private
 
-      def cache_key_for(config)
-        "#{config.tenant_id}:#{config.client_id}"
+      def token_expired?
+        return true unless @token_expires_at
+
+        Time.now.to_i >= (@token_expires_at - EXPIRY_BUFFER_SECONDS)
       end
 
-      def token_expired?(token_data)
-        return true unless token_data[:expires_at]
+      def fetch_token
+        validate_config!
 
-        Time.now.to_i >= (token_data[:expires_at] - EXPIRY_BUFFER_SECONDS)
-      end
-
-      def fetch_token(config)
-        validate_config!(config)
-
-        uri = URI(format(TOKEN_URL, tenant_id: config.tenant_id))
+        uri = URI(format(TOKEN_URL, tenant_id: @tenant_id))
 
         request = Net::HTTP::Post.new(uri)
         request["Content-Type"] = "application/x-www-form-urlencoded"
         request.body = URI.encode_www_form(
-          client_id: config.client_id,
-          client_secret: config.client_secret,
+          client_id: @client_id,
+          client_secret: @client_secret,
           scope: GRAPH_SCOPE,
           grant_type: "client_credentials"
         )
@@ -107,11 +90,11 @@ module OpenProject
         raise TokenError, "Failed to obtain access token: #{e.message}"
       end
 
-      def validate_config!(config)
+      def validate_config!
         missing = []
-        missing << "tenant_id" if config.tenant_id.blank?
-        missing << "client_id" if config.client_id.blank?
-        missing << "client_secret" if config.client_secret.blank?
+        missing << "tenant_id" if @tenant_id.blank?
+        missing << "client_id" if @client_id.blank?
+        missing << "client_secret" if @client_secret.blank?
 
         raise TokenError, "Missing configuration: #{missing.join(', ')}" if missing.any?
       end
@@ -124,10 +107,8 @@ module OpenProject
           raise TokenError, "Token request failed: #{error}"
         end
 
-        {
-          access_token: body["access_token"],
-          expires_at: Time.now.to_i + body["expires_in"].to_i
-        }
+        @cached_token = body["access_token"]
+        @token_expires_at = Time.now.to_i + body["expires_in"].to_i
       end
     end
   end
